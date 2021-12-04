098a5b69718b67217d4f7fda747dd250e372a4cd
JC <james.calingo@gmail.com>
2021-12-04 09:54:57 -0500

    changing link

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Link from "next/Link"
+import Link from "next/link"
 import { Icon } from "@iconify/react";
 import githubIcon from "@iconify/icons-simple-icons/github";
 import linkedinIcon from "@iconify/icons-simple-icons/linkedin";
